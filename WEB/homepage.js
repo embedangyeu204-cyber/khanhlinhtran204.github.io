85ad3bbaf0bd5bd7dd92ee12296fc013ad4d789c
embedangyeu204-cyber <22070181@vnu.edu.vn>
2025-11-04 19:58:21 +0700

    pet shop
    hhhhh

---
+++ WEB/homepage.js
@@ -0,0 +1,8 @@
+// Tab switching
+const tabs = document.querySelectorAll('.tabs button');
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    tabs.forEach(t => t.classList.remove('active'));
+    tab.classList.add('active');
+  });
+});
